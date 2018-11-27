ad502a4ed414a70b8e3778244c33009d65162378
2001SameerSharma <45395094+2001SameerSharma@users.noreply.github.com>
2018-11-27 10:19:55 -0500

    Combine two ArrayList
    Wrote the method and a test to combine two ArrayLists

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -1,16 +1,42 @@
+import java.util.ArrayList;
 public class ArrayListDemo{
 
+
+
+	public static ArrayList<Integer> combine(ArrayList<Integer> one, ArrayList<Integer> two){
+
+		ArrayList<Integer> combined = new ArrayList<Integer>();
+
+		for(int i=0; i<one.size(); i++){
+			combined.add(one.get(i));
+		}
+
+		for(int i=0; i<two.size(); i++){
+			combined.add(two.get(i));
+		}
+
+		return combined;
+
+}
+
+
 	public static void main(String [] args){
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	ArrayList<Integer> a = new ArrayList<Integer>();
+	ArrayList<Integer> b = new ArrayList<Integer>();
+
+	a.add(1);
+	b.add(1);
+
+	System.out.println(combine(a,b));
+
+
+
+
+
+
+
+
+
 	}
 }
